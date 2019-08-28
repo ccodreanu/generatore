bb618a0baf0bff3618888d89cce7110e7e23a7cb
Catalin Codreanu <cc.catalincodreanu@gmail.com>
2019-08-28 15:38:43 +0200

    take care of some punctuation in slugs

--- generatore/Utils.py
+++ generatore/Utils.py
@@ -1,8 +1,11 @@
+import re
+
 def slug_creator(weird_string):
     """
     This creates slugs starting in the ASCII character space.
     """
     slug = weird_string.encode('ascii', 'ignore').lower().decode()
     slug = slug.replace(' ', '-')
+    slug = re.sub(r'!|,|\*|\?|\.', r'', slug)
 
     return slug
