0ee242a572df086a67b393b9b860cfafd7f1c885
Catalin Codreanu <cc.catalincodreanu@gmail.com>
2019-08-28 14:33:55 +0200

    output dir is going to be a param

--- generatore/ArticleCreator.py
+++ generatore/ArticleCreator.py
@@ -8,6 +8,7 @@ from generatore.Utils import slug_creator
 class ArticleCreator():
     def __init__(self, filename):
         article = Content(filename)
+        output_dir = 'output/'
 
         file_loader = FileSystemLoader(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'))
         env = Environment(loader=file_loader)
@@ -15,5 +16,5 @@ class ArticleCreator():
         template = env.get_template('article.html')
         output = template.render(content=article)
         
-        with open('output/' + slug_creator(article.metadata['title']) + '.html', 'w') as writer:
+        with open(output_dir + slug_creator(article.metadata['title']) + '.html', 'w') as writer:
             writer.write(output)
