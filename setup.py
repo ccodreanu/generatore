9b5fc36c833307d7f852dd20ee91af2e7da0c759
Catalin Codreanu <cc.catalincodreanu@gmail.com>
2019-08-29 17:58:11 +0200

    run is not needed anymore

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/picofish/generatore",
     packages=setuptools.find_packages(),
+    scripts=['bin/generatore'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
