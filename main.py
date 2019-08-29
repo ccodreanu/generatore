b1fbf326a1fdc66f35fb57bb8482624a2450cad6
Catalin Codreanu <cc.catalincodreanu@gmail.com>
2019-08-29 11:37:20 +0200

    arguments for app

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
+import sys
+import http.server
+import socketserver
+import os
+
 from generatore.SiteBuilder import SiteBuilder
 
-filename = 'content/posts/Running-Wireshark-on-remote-hosts.md'
+def usage():
+    print('usage: {} <command>\n'.format(sys.argv[0]))
+    print('Generatore creates static websites.\n')
+    print('Available commands:')
+    print('{:>10}\t{}'.format('build', 'build the site inside the output directory'))
+    print('{:>10}\t{}'.format('serve', 'generate content and start a webserver'))
 
-# c = Content(filename)
+site = SiteBuilder()
 
-# print(c.body)
-# print(c.metadata['title'])
+if (len(sys.argv) > 1):
+    if (sys.argv[1] == 'build'):
+        site.build_site()
+    elif (sys.argv[1] == 'serve'):
+        site.build_site()
 
-# file_loader = FileSystemLoader('template')
-# env = Environment(loader=file_loader)
-
-# template = env.get_template('article.html')
-# output = template.render(content=c)
-# print(output)
-
-# g = ArticleCreator(filename)
-
-s = SiteBuilder()
-
-s.build_site()
+        web_dir = os.path.join(os.path.dirname(__file__), 'output')
+        os.chdir(web_dir)
+        with socketserver.TCPServer(("", 9999), http.server.SimpleHTTPRequestHandler) as httpd:
+            print("serving at port", 9999)
+            httpd.serve_forever()
+    else:
+        usage()
+else:
+    usage()
