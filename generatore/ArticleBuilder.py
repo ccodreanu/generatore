d52e2f1926300aa781f8e252cbe331f2ef1dfbd7
Catalin Codreanu <cc.catalincodreanu@gmail.com>
2019-09-02 23:07:02 +0200

    new classes

---
+++ generatore/ArticleBuilder.py
@@ -0,0 +1,33 @@
+import os
+
+from datetime import datetime
+
+from jinja2 import Environment, FileSystemLoader
+
+from generatore.Content import Content
+from generatore.Utils import slug_creator
+
+class ArticleBuilder(Content):
+    def __init__(self, filename, destination, config):
+        super().__init__(filename)
+
+        self.config = config
+
+        self.destination = destination
+
+    def write(self, pages):
+        output_dir = self.destination
+
+        file_loader = FileSystemLoader(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'))
+        env = Environment(loader=file_loader)
+
+        template = env.get_template('article.html')
+        output = template.render(site_name=self.config['site_name'], google_analytics=self.config['google_analytics'], title=self.metadata['title'], date=self.metadata['date'], content=self.body, pages=pages)
+        
+        with open(os.path.join(output_dir, self.document_url), 'w') as writer:
+            writer.write(output)
+
+    @staticmethod
+    def new_skeleton(title):
+        with open(os.path.join(os.getcwd(), 'content', 'posts', slug_creator(title) + '.md'), 'w+') as writer:
+            writer.write('---\ntitle: {}\ndate: {}\n---\n'.format(title, datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
---
+++ generatore/ConfigManager.py
@@ -0,0 +1,22 @@
+import json
+import os
+
+class ConfigManager():
+    def __init__(self, path=os.getcwd()):
+        self.config_path = os.path.join(os.path.abspath(path), 'config')
+    
+    def read(self):
+        with open(self.config_path, 'r') as config_file:
+            config = json.load(config_file)
+
+            return config
+
+    def write(self, config = []):
+        default_config = {
+            "site_name": "New site"
+        }
+
+        default_config.update(config)
+
+        with open(self.config_path, 'w') as config_file:
+            json.dump(default_config, config_file)
