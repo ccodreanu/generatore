a41a7104fae835d689ea94330a6f65439264e8c8
Catalin Codreanu <cc.catalincodreanu@gmail.com>
2019-09-03 09:12:53 +0200

    utils doc

--- generatore/Utils.py
+++ generatore/Utils.py
@@ -2,7 +2,7 @@ import re
 
 def slug_creator(weird_string):
     """
-    This creates slugs starting in the ASCII character space.
+    This creates slugs in the ASCII character space.
     """
     slug = weird_string.encode('ascii', 'ignore').lower().decode()
     slug = slug.replace(' ', '-')
