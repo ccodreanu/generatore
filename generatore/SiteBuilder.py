930171ccb2342aaf0dbfe58173dd1733d97e513f
Catalin Codreanu <cc.catalincodreanu@gmail.com>
2019-09-03 10:05:59 +0200

    moving the listener

--- generatore/SiteBuilder.py
+++ generatore/SiteBuilder.py
@@ -1,13 +1,27 @@
 import os
+import time
 
 from distutils.dir_util import copy_tree
 from jinja2 import Environment, FileSystemLoader
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 from generatore.ArticleBuilder import ArticleBuilder
 from generatore.ConfigManager import ConfigManager
 
+
+class ContentHandler(FileSystemEventHandler):
+    def __init__(self, site):
+        self.site = site
+
+    def on_modified(self, event):
+        print(f'{event.src_path} has been {event.event_type}')
+        self.site.build_site()
+
+
 class SiteBuilder:
-    def __init__(self, dir=os.getcwd(), content_dir="content", output_dir="output"):
+    def __init__(self, dir=os.getcwd(), content_dir="content",
+                 output_dir="output"):
         self.dir = os.path.abspath(dir)
 
         self.config = ConfigManager(dir).read()
@@ -29,21 +43,40 @@ class SiteBuilder:
 
     def build_site(self):
         self.pages = []
-        self.pages = self.__generate_content__(self.pages_content_dir, self.pages_output_dir)
+        self.pages = self.__generate_content__(self.pages_content_dir,
+                                               self.pages_output_dir)
         self.pages.sort(key=lambda post: post.metadata['date'], reverse=True)
 
-        self.posts = self.__generate_content__(self.posts_content_dir, self.posts_output_dir)
-        
+        self.posts = self.__generate_content__(self.posts_content_dir,
+                                               self.posts_output_dir)
+
         # sort for sitemap
         self.posts.sort(key=lambda post: post.metadata['date'], reverse=True)
-        
+
         [post.write(self.pages) for post in self.posts]
         [page.write(self.pages) for page in self.pages]
 
         self.__generate_index__()
 
         # Copy over static files
-        copy_tree(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates', 'static'), self.static_output_dir)
+        copy_tree(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                  'templates', 'static'), self.static_output_dir)
+
+    def auto_build_site(self):
+        content_handler = ContentHandler(self)
+        observer = Observer()
+        observer.schedule(content_handler, path=os.path.abspath('content'),
+                          recursive=True)
+        observer.start()
+
+        print('Listening for changes...')
+
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
 
     def __create_site_structure__(self):
         if not os.path.exists(self.posts_content_dir):
